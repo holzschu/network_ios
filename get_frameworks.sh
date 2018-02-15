a34eed64da4cec03406a0e31230fcafb7b289006
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-15 14:12:08 +0100

    More robust location for ios_error.h

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -7,13 +7,13 @@ HHROOT="https://github.com/holzschu"
 
 (cd "${BASH_SOURCE%/*}/Frameworks"
 # ios_system
-echo "Downloading ios_system:"
+echo "Downloading ios_system, openSSL and libSSH2:"
 curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/smallRelease.tar.gz
 ( tar -xzf smallRelease.tar.gz --strip 1 && rm smallRelease.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
 )
 
 echo "Downloading header file:"
-curl -OL https://raw.githubusercontent.com/holzschu/ios_system/master/ios_error.h
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_error.h 
 
 
 
