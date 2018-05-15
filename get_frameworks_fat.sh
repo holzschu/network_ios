8cbbe9722b905ce43200f3b68bb177e8810a456f
Yury Korolev <yury.korolev@gmail.com>
2018-05-15 10:33:13 +0300

    Grab ios_system framework

--- get_frameworks_fat.sh
+++ get_frameworks_fat.sh
@@ -7,6 +7,10 @@ HHROOT="https://github.com/holzschu"
 
 (cd "${BASH_SOURCE%/*}/Frameworks"
 # ios_system
+echo "Downloading ios_system, openSSL and libSSH2:"
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/smallRelease.tar.gz
+( tar -xzf smallRelease.tar.gz --strip 1 && rm smallRelease.tar.gz ) || { echo "ios_system failed to download"; exit 1; }
+
 echo "Downloading fat frameworks:"
 curl -OL $HHROOT/libssh2-for-iOS/releases/download/v$LIBSSH2_VER/release.tar.gz
 ( tar -xzf release.tar.gz && rm release.tar.gz ) || { echo "libssh2 failed to download"; exit 1; }
