874e0b345dcc3532bc2c596c455d2b287c025260
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-07 22:38:33 +0200

    Updated release location

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "network_ios",
-            url: "https://github.com/holzschu/network_ios/releases/download/1.0/network_ios.xcframework.zip",
+            url: "https://github.com/holzschu/network_ios/releases/download/0.2/network_ios.xcframework.zip",
             checksum: "61ff7ab2aa17dc4259bad795dc016a82c25dde13c3a0f461f408143d51463711"
         )
     ]
