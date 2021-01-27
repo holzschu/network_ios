eff02550d62db84800c3ec955f8b0785f26b57d4
Yury Korolev <yury.korolev@gmail.com>
2021-01-27 13:26:35 +0300

    Update build

--- xcfs/Package.swift
+++ xcfs/Package.swift
@@ -5,14 +5,14 @@ _ = Package(
     name: "deps",
     platforms: [.macOS("11")],
     dependencies: [
-        .package(url: "https://github.com/yury/FMake", from: "0.0.8")
+        .package(url: "https://github.com/yury/FMake", from: "0.0.15")
     ],
     
     targets: [
         .binaryTarget(
             name: "ios_system",
-            url: "https://github.com/holzschu/ios_system/releases/download/2.7/ios_system.xcframework.zip",
-            checksum: "9f60fec58e4354fe8b9568ef1b2f252c77e1b15abc011636b81ec04b5fca8c41"
+            url: "https://github.com/yury/ios_system/releases/download/v2.7.0/ios_system.xcframework.zip",
+            checksum: "a3ec1c198b944b7d0e8932b7da447b164ae9b869c11c8f0f35cc7b5f09129fe2"
         ),
 
         .target(
--- xcfs/Sources/build/main.swift
+++ xcfs/Sources/build/main.swift
@@ -7,21 +7,12 @@ import FMake
 OutputLevel.default = .error
 
 enum Config {
-    static let iOSSystemVersion = "2.6"
+    static let iOSSystemVersion = "2.7"
     static let iOSSystemRoot    = "https://github.com/holzschu"
 
     static var iOSSystemErrorHURL: String {
         "\(iOSSystemRoot)/ios_system/releases/download/\(iOSSystemVersion)/ios_error.h"
     }
-}
-
-extension Platform {
-  var excludedArchs: [Platform.Arch] {
-      switch self {
-          case .iPhoneSimulator: return [.arm64] // TODO: add arm64 simulator
-          default: return []
-      }
-  }
 }
 
 try download(url: Config.iOSSystemErrorHURL)
@@ -31,22 +22,21 @@ let platforms: [Platform] = [.iPhoneOS, .iPhoneSimulator]
 
 let schemes = [ "network_ios" ]
 
-var checksums: [(file: String, value: String)] = []
+var checksums: [[String]] = []
 
 for scheme in schemes {
     try xcxcf(
         dirPath: ".build",
         project: "network_ios",
         scheme: scheme,
-        platforms: platforms.map { ($0, excludedArchs: $0.excludedArchs) },
-        enableBitCode: false
+        platforms: platforms.map { ($0, excludedArchs: []) }
     )
 
     try cd(".build") {
         let zip = "\(scheme).xcframework.zip"
         try sh("zip -r \(zip) \(scheme).xcframework")
         let chksum = try sha(path: zip)
-        checksums.append((file: zip, value: chksum))
+        checksums.append([zip, chksum])
     }
 }
 
@@ -54,11 +44,7 @@ var releaseNotes =
 """
 Release notes:
 
-    | File                            | SHA 256                                             |
-    | ------------------------------- |:---------------------------------------------------:|
-\(checksums.map {
-    "    | \($0.file) | \($0.value) |"
-}.joined(separator: "\n"))
+\( checksums.markdown(headers: "File", "SHA 256") )
 
 """
 
