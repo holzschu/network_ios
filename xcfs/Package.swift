a1140568cb23616b89c1ecfe585e526d9db39d79
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2023-12-30 16:58:11 +0100

    Changed FMake location for building

--- xcfs/Package.swift
+++ xcfs/Package.swift
@@ -5,7 +5,7 @@ _ = Package(
     name: "xcfs",
     platforms: [.macOS("11")],
     dependencies: [
-        .package(url: "https://github.com/yury/FMake", from: "0.0.16")
+        .package(url: "https://github.com/blinksh/FMake", from: "0.0.16")
     ],
     
     targets: [
